10160cc36a3305ad2d45cb6d451164b1e12daf00
oleg-pyshenkin <olegpyshenkin.git@gmail.com>
2026-01-20 21:25:27 -0800

    Add GitHub Actions CI workflow

--- api_client.py
+++ api_client.py
@@ -1,24 +1,51 @@
 import requests
-from config import BASE_URL
+import allure
+import json
 
 class PostClient:
     def __init__(self, base_url):
         self.url = f"{base_url}/posts"
 
+    def _attach_details(self, response):
+        allure.attach(
+            f"Method: {response.request.method}\nURL: {response.url}",
+            name="Request Info",
+            attachment_type=allure.attachment_type.TEXT
+        )
+        allure.attach(
+            json.dumps(response.json(), indent=4),
+            name="Response Body",
+            attachment_type=allure.attachment_type.JSON
+        )
+
+
     def get_one(self, post_id):
-        return requests.get(f"{self.url}/{post_id}")
+        response = requests.get(f"{self.url}/{post_id}")
+        self._attach_details(response)
+        return response
     
     def create(self, payload):
-        return requests.post(self.url, json=payload)
+        response = requests.post(self.url, json=payload)
+        self._attach_details(response)
+        return response
     
     def get_all(self):
-        return requests.get(self.url)
+        response = requests.get(self.url)
+        self._attach_details(response)
+        return response
+
     
     def delete(self, post_id):
-        return requests.delete(f"{self.url}/{post_id}")
+        response = requests.delete(f"{self.url}/{post_id}")
+        self._attach_details(response)
+        return response
 
     def update (self, post_id, payload):
-        return requests.put(f"{self.url}/{post_id}", json=payload)
+        response = requests.put(f"{self.url}/{post_id}", json=payload)
+        self._attach_details(response)
+        return response
     
     def get_comments(self, post_id):
-        return requests.get(f"{self.url}/{post_id}/comments")
+        response = requests.get(f"{self.url}/{post_id}/comments")
+        self._attach_details(response)
+        return response
